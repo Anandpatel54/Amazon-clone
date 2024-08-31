0276e96e0ca27f5343351a0c9228572599e9a44e
Anand patel <panand054@gmail.com>
2024-08-31 20:01:34 +0530

    Done

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -16,31 +16,32 @@ const HomePage = () => {
     <div>
       <Image
         style={{
-          maskImage:
-            "linear-gradient(to bottom, rgba(0,0,0,1), rgba(0,0,0,0.5))",
+          maskImage: "linear-gradient(to bottom, rgba(0,0,0,1), rgba(0,0,0,0))",
         }}
         src={
-          "https://images-eu.ssl-images-amazon.com/images/G/31/img17/Home/AmazonTV/YMF/YMF_S4/3000x1200_V1._CB567147650_.jpg"
+          "https://images-eu.ssl-images-amazon.com/images/G/31/img24/AmazonPay/Travel/PC_Hero_BAU/IF_PC_Hero_3000x1200._CB583399235_.jpg"
         }
-        height={1000}
         width={10000}
-        alt=""
+        height={1000}
+        alt="banner"
       />
-      <div className="w-[85%] mx-auto grid grid-cols-4 gap-4 relative -top-64">
-        {mensProduct.map((product) => (
-          <div key={product.id}>
-            <CategoryWiseProduct product={product} />
-          </div>
-        ))}
-        {womensProduct.map((product) => (
-          <div key={product.id}>
-            <CategoryWiseProduct product={product} />
-          </div>
-        ))}
+      <div className="w-[90%] mx-auto grid grid-cols-4 gap-2 relative -top-64">
+        {mensProduct.map((product: any) => {
+          return (
+            <div key={product.id}>
+              <CategoryWiseProduct product={product} />
+            </div>
+          );
+        })}
+        {womensProduct.map((product: any) => {
+          return (
+            <div key={product.id}>
+              <CategoryWiseProduct product={product} />
+            </div>
+          );
+        })}
       </div>
-      <Footer />
     </div>
   );
 };
-
 export default HomePage;
